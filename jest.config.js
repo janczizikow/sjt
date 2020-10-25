edc4849f5c2fcf32bac8e816f9b751a9dc812654
Jan Czizikow <jan.czizikow@gmail.com>
2020-10-25 22:57:31 +0100

    chore: specify files for code coverage (#5)
    * chore: specify files for code coverage
    
    * fix: don't collect coverage from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  collectCoverageFrom: [
+    '**/*.js',
+    '!jest.config.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+  ],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
